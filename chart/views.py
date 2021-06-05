02295e8d41e3c43bc6913126df6dc9dc196c2130
HUe1998 <himanshu.1998me@gmail.com>
2021-06-05 18:27:31 +0530

    Parse url JSON into required arrays

--- chart/views.py
+++ chart/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, JsonResponse
+from urllib.request import urlopen
+import json
 
 
 def ChartView(request):
     url = 'https://api.covid19api.com/summary'
-    return HttpResponse('Hello World!')
+    data_json = json.loads(urlopen(url).read())
+    countryList = []
+    totalList = []
+    for country in data_json['Countries']:
+        countryList.append(country['Country'])
+        totalList.append(country['TotalConfirmed'])
+    return JsonResponse(data_json)
